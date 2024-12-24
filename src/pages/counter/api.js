60f2c7f4da3561b2e008b7a1923fcc239cd84607
Daiana Makushina <dayana@192.168.1.4>
2024-12-24 12:56:40 +0100

    delete files for gh-pages

--- src/pages/counter/api.js
+++
@@ -1,8 +0,0 @@
-import { client } from "../../api/client";
-
-export const API = {
-  getCounter: function() {
-    console.log('API.getCounter called');
-    return client.get('/api/counter')
-  }
-}
--- src/pages/counter/constants.js
+++
@@ -1,7 +0,0 @@
-export const COUNTER_INCREMENT = 'counter/increment';
-export const COUNTER_DECREMENT = 'counter/decrement';
-export const COUNTER_RESET = 'counter/reset';
-
-export const COUNTER_LOADING = 'counter/loading';
-export const COUNTER_LOADING_SUCCES = 'counter/loading/succes';
-export const COUNTER_LOADING_ERROR = 'counter/loading/error';
